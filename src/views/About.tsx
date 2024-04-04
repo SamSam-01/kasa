40ee5931345b73069f33fc5c54bd1f8d3b00588f
Samuel BLARD <samuel.blard2@gmail.com>
2024-04-04 14:57:20 +0200

    add responsive for Logement page and text for about page

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -7,10 +7,12 @@ const About: React.FC = () => {
   return (
     <div className='about_page'>
       <Banner image={process.env.PUBLIC_URL + "/assets/about_banner.png"}/>
-      <Collapse title='Fiabilité' content={"hi"} />
-      <Collapse title='Respect' content={"hi"} />
-      <Collapse title='Service' content={"hi"} />
-      <Collapse title='Sécurité' content={"hi"} />
+      <Collapse title='Fiabilité' content={"Les annonces postées sur Kasa garantissent une flabilité totale. Les photos sont conformes aux logements, et toutes les informations sont réguliérement vérifiées par nos équipes."} />
+      <Collapse title='Respect' content={"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme."} />
+      <Collapse title='Service' content={"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme."} />
+      <Collapse title='Sécurité' content={"La sécurité est la priorité de Kasa; Aussi bien pour nos hôtes que pour les voyageurs, \
+      chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, \
+      cela permet nos équipes de verifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."} />
     </div>
   );
 };
